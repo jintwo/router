556331bda41c10565b5813ae8f2aacbd58d57bbd
Eugeny Volobuev <qulert@gmail.com>
2024-11-21 07:24:09 +0800

    Initial Commit

---
+++ Router/RouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouterApp.swift
+//  Router
+//
+//  Created by Eugeny Volobuev on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct RouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
